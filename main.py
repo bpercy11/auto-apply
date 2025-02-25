1ea8abb0066bd26d14b88cc559e862a87b6c5e47
brett percy <bp11@gmail.com>
2025-02-25 16:56:10 -0600

    add headers to mimic browser and bypass 403 errors when hitting website

--- main.py
+++ main.py
@@ -73,7 +73,10 @@ if __name__ == "__main__":
         job_description_url = input("Job description URL: ")
 
         if job_description_url:
-            response = requests.get(job_description_url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+            response = requests.get(job_description_url, headers=headers)
             response.raise_for_status()
             soup = BeautifulSoup(response.content, 'html.parser')
             job_description_text = soup.get_text(separator=' ', strip=True)
